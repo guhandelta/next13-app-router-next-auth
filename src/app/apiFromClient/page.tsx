4d9220f9fb4bf51d8c971522c8bd3679c406f86a
guhandelta <guhan.delta@gmail.com>
2023-09-07 10:49:43 -0400

    Created Protected API Routes

---
+++ src/app/apiFromClient/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { useEffect, useState } from "react";
+
+export default function APITestPage() {
+    const [name, setName] = useState<string>();
+
+    useEffect(() => {
+        fetch("/api/whoAmI")
+            .then((res) => res.json())
+            .then((data) => { setName(data.name); });
+    }, []);
+
+    return (
+        <div className="">
+            <div className="text-center">
+                API Route from
+                <span className="font-bold underline">
+                    Client
+                </span>
+            </div>
+            <div className="">
+                Name: {name}
+            </div>
+        </div>
+    );
+}
